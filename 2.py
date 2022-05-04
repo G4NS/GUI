ee1f14546f4eb9849cefe47a470c0934250d2882
G4NS <62437842+G4NS@users.noreply.github.com>
2022-05-05 02:22:08 +0600

    second commit

--- 2.py
+++ 2.py
@@ -1,54 +1,26 @@
-#не работает
-
-
 import tkinter as tk
-
-class trans:
-    def __init__(self):
-        self.window = tk.Tk()
-
-        self.trans_frame = tk.Frame(self.window)
-        self.buttons_frame = tk.Frame(self.window)
-
-        self.btn_sinister = tk.Button(text="sinister", command=self.sinister)
-        self.btn_dexter = tk.Button(text="dexter", command=self.dexter)
-        self.btn_medium = tk.Button(text="medium", command=self.medium)
-
-        self.btn_sinister.pack(side="left")
-        self.btn_dexter.pack(side="left")
-        self.btn_medium.pack(side="left")
-
-        self.trans_frame.pack()
-        self.buttons_frame.pack()
-
-        #self.window.geometry('400x120') #Задаем своии размеры окна
-        self.window.mainloop()
-
-    def sinister(self):
-        self.a = "Левый"
-        Label = tk.Label(text=self.a, foreground="black", bg="white")
-        Label.pack(side="top")
-
-    def dexter(self):
-        self.a = "Правый"
-        Label = tk.Label(text=self.a, foreground="black")
-        Label.pack(side="top")
-
-    def medium(self):
-        self.a = "Центральный"
-        Label = tk.Label(text=self.a, foreground="black")
-        Label.pack(side="top")
-
-
-#trans = trans()
-
-
-
-
 window = tk.Tk()
-window.geometry("300x100")
+window.geometry("190x50")
 
-tk.Button(text="sinister").grind(row=1, column=0)
+tk.Label(text="                          ", foreground="black").grid(row=0, column=1)
 
+def sinister():
+    a = "Левый"
+    tk.Label(text="                          ", foreground="black").grid(row=0, column=1)
+    tk.Label(text=a, foreground="black").grid(row=0, column=1)
+
+def dexter():
+    a = "Правый"
+    tk.Label(text="                          ", foreground="black").grid(row=0, column=1)
+    tk.Label(text=a, foreground="black").grid(row=0, column=1)
+
+def medium():
+    a = "Центральный"
+    tk.Label(text="                          ", foreground="black").grid(row=0, column=1)
+    tk.Label(text=a, foreground="black").grid(row=0, column=1)
+
+tk.Button(text="sinister", command=sinister).grid(row=2, column=0, stick='wens')
+tk.Button(text="dexter", command=dexter).grid(row=2, column=1, stick='wens')
+tk.Button(text="medium", command=medium).grid(row=2, column=2, stick='wens')
 
 window.mainloop()
